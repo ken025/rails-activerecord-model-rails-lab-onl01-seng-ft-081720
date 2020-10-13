e72157fccb4ad6a35cedd43a11c19835eec4ca06
Kenneth Colon <kennethmanuel250@gmail.com>
2020-10-13 04:09:00 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_students_table.rb
+++ db/migrate/001_create_students_table.rb
@@ -1,6 +1,7 @@
 class Student < ActiveRecord::Migration
   
-  def create_table :students do |t| 
+  def change
+    create_table :students do |t| 
     t.string :first_name
     t.string :last_name 
   end 
